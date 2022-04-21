0807e857fb18bba8b9bca5426a5eb335f191d1e1
Xander Morgan <xmorgan@usc.edu>
2022-04-21 12:35:42 -0700

    util modifications to avoid IOS issues

--- util.py
+++ util.py
@@ -2,5 +2,8 @@ def get_id_from_tag(tag: str):
     try:
         return int(tag.strip().removeprefix("<@").removesuffix(">"))
     except ValueError:
-        return None
+        try:
+            return int(tag.strip().removeprefix("<@!").removesuffix(">"))
+        except ValueError:
+            return None
 
